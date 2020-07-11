16343d7b4f97ec63b5cff533694dd73cee792d7a
jamowei <jan@rexster.de>
2020-07-11 12:07:37 +0200

    adjust validation example

--- validation/src/commonMain/kotlin/dev/fritz2/examples/validation/model.kt
+++ validation/src/commonMain/kotlin/dev/fritz2/examples/validation/model.kt
@@ -3,23 +3,21 @@ package dev.fritz2.examples.validation
 import com.soywiz.klock.*
 import dev.fritz2.format.Format
 import dev.fritz2.lenses.Lenses
-import dev.fritz2.lenses.WithId
 
 @Lenses
 data class Person(
-    override val id: String,
     val name: String = "",
     val birthday: Date = Date(1900, 1, 1),
     val address: Address = Address(),
     val activities: List<Activity> = listOf(
-        Activity("ac1", "walking"),
-        Activity("ac2", "running"),
-        Activity("ac3", "meeting friends"),
-        Activity("ac4", "playing computer games"),
-        Activity("ac5", "programming"),
-        Activity("ac6", "to go biking")
+        Activity("walking"),
+        Activity("running"),
+        Activity("meeting friends"),
+        Activity("playing computer games"),
+        Activity("programming"),
+        Activity("to go biking")
     )
-) : WithId
+)
 
 @Lenses
 data class Address(
@@ -31,10 +29,9 @@ data class Address(
 
 @Lenses
 data class Activity(
-    override val id: String,
     val name: String,
     val like: Boolean = false
-) : WithId
+)
 
 object Format {
     val date = object : Format<Date> {
